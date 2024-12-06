5909325730d0daacfeadc134decd5dbf64e2e882
Maurice Ihl <maurice.ihl1996@gmail.com>
2024-12-06 13:13:07 +0100

    extended props

--- src/blocks/Button/Component.tsx
+++ src/blocks/Button/Component.tsx
@@ -1,13 +1,28 @@
 import { Button } from '@/components/ui/button'
-import { ButtonType } from '@/payload-types'
+import { ButtonType, Media } from '@/payload-types'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const BaseplButton = (props: ButtonType) => {
-  const { id, size, variant, isIconStart, hasIcon, link } = props
-  const { label, externalUrl, emailAddress, slug, pageReference } = link
+  const { id, size, variant, isIconStart, hasIcon, icon, link } = props
+  const {
+    label,
+    externalUrl,
+    emailAddress,
+    slug,
+    useSlug,
+    pageReference,
+    isOpenNewTab,
+    hasNoReferrer,
+  } = link
+
+  const target = externalUrl && isOpenNewTab ? '_blank' : undefined
+  const reffererPolicy = hasNoReferrer ? 'no-referrer' : undefined
+  const iconUrl = (icon as Media)?.url ?? '/'
+  const alt = (icon as Media)?.alt ?? label
 
   const getHref = () => {
-    if (slug) {
+    if (slug && useSlug) {
       return slug
     }
     if (externalUrl) {
@@ -24,7 +39,11 @@ export const BaseplButton = (props: ButtonType) => {
 
   return (
     <Button variant={variant} size={size} id={id ?? ''} asChild>
-      <Link href={getHref()}></Link>
+      {hasIcon && isIconStart && <Image src={alt} alt={label} fill />}
+      <Link href={getHref()} target={target} referrerPolicy={reffererPolicy}>
+        {label}
+      </Link>
+      {hasIcon && <Image src={iconUrl} alt={alt} fill />}
     </Button>
   )
 }
