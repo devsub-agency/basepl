9ec0fad144509db883ecbd64abe30c55a9a0583c
Maurice Ihl <maurice.ihl1996@gmail.com>
2024-12-14 10:08:54 +0100

    merged richtext

--- www/basepl/src/blocks/BaseplRichtext/Component.tsx
+++ www/basepl/src/blocks/BaseplRichtext/Component.tsx
@@ -1,7 +1,39 @@
 import { BaseplRichtextType } from '@/payload-types'
 import RichText from './RichText'
+import { BaseplButtonType, BaseplImageType, BaseplVideoType } from '@/payload-types'
+import { BaseplButton } from '../BaseplButton/Component'
+import { BaseplImage } from '../BaseplImage/Component'
+import { BaseplVideo } from '../BaseplVideo/Component'
+import {
+  JSXConvertersFunction,
+  RichText as RichTextWithoutBlocks,
+} from '@payloadcms/richtext-lexical/react'
+import { DefaultNodeTypes, SerializedBlockNode } from '@payloadcms/richtext-lexical'
+import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
+import { cn } from '@/lib/utils'
+
+type NodeTypes =
+  | DefaultNodeTypes
+  | SerializedBlockNode<BaseplButtonType | BaseplVideoType | BaseplImageType>
+
+const jsxConverters: JSXConvertersFunction<NodeTypes> = ({ defaultConverters }) => ({
+  ...defaultConverters,
+  blocks: {
+    baseplImage: ({ node }) => <BaseplImage {...node.fields} />,
+    baseplVideo: ({ node }) => <BaseplVideo {...node.fields} />,
+    baseplButton: ({ node }) => <BaseplButton {...node.fields} />,
+  },
+})
 
 export const BaseplRichtext = (props: BaseplRichtextType) => {
   const { content } = props
-  return <RichText content={content} />
+
+  return (
+    <RichTextWithoutBlocks
+      // @ts-expect-error
+      converters={jsxConverters}
+      className={cn()}
+      {...{ data: content as SerializedEditorState }}
+    />
+  )
 }
